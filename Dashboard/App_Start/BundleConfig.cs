042d16d1e8200fc27c9e573d2e48634fa657677d
louishol <louis_hol@hotmail.com>
2016-02-03 15:48:02 +0100

    Dashboard index

--- Dashboard/App_Start/BundleConfig.cs
+++ Dashboard/App_Start/BundleConfig.cs
@@ -11,6 +11,8 @@ namespace Dashboard
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -26,6 +28,10 @@ namespace Dashboard
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/chart").Include(
+                  "~/Content/jquery.jqplot.css"));
+
         }
     }
 }
